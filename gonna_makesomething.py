1c7ebb36fee8cf81908949d97cc4d809bfcc5df2
Glitch1907 <shreyaspandiri@gmail.com>
2026-06-30 10:17:56 +0530

    added something into the branch

--- gonna_makesomething.py
+++ gonna_makesomething.py
@@ -5,3 +5,5 @@ else :
     print("its not worthied doing shit")
     for i in range[1:19]:
         print(i)
+
+#adding a new line from here i am checking how does a branch work and what do i need to do ;-;
