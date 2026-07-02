1558ddad538d62b092ac53075315b00393d5a7e8
Glitch1907 <shreyaspandiri@gmail.com>
2026-07-02 16:29:14 +0530

    added a for loop

--- gonna_makesomething.py
+++ gonna_makesomething.py
@@ -3,7 +3,11 @@ if a is "is" :
     print("very good")
 else :
     print("its not worthied doing shit")
-    for i in range[1:19]:
+    for i in range(4,19):
         print(i)
 
 #adding a new line from here i am checking how does a branch work and what do i need to do ;-;
+
+
+for i in range(10):
+    print(i,"lol")
